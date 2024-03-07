11ea1ea645c0e73bef9b5b1e92109fa8ab8ad389
Bismark Aidoo <mc07.aidoo@gmail.com>
2024-03-07 11:23:09 +0000

    api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x016.project:v1.0.0 (by /u/BismarkAidoo)"
+        "User-Agent": "Linux:0x016.project:v1.0.0 (by /u/BismarkAidoo)"
         }
     subreddit_data = requests.get(url,
                                   headers=headers,
